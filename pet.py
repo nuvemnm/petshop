8deee644a9933440890cb11da06f408db43df1f6
nuvemnm <matheuscarv010@gmail.com>
2025-01-05 16:28:55 -0300

    feat: inicio do projeto

--- pet.py
+++ pet.py
@@ -1,28 +1,38 @@
 import flet as ft
 
-def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
-
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
+def home_page(page: ft.Page):
+    return ft.Column(
+        [
+            ft.Text("Página Inicial", size=30),
+            ft.ElevatedButton("Ir para Página 2", on_click=lambda _: page.go("/page2")),
+        ]
     )
 
-ft.app(main)
+def page2(page: ft.Page):
+    return ft.Column(
+        [
+            ft.Text("Página 2", size=30),
+            ft.ElevatedButton("Voltar para Página Inicial", on_click=lambda _: page.go("/")),
+        ]
+    )
+
+
+def main(page: ft.Page):
+    page.title = "Navegação com Flet"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    # Função de roteamento
+    def route_change(route):
+        if page.route == "/":
+            page.views.clear()
+            page.views.append(ft.View("/", [home_page(page)]))
+        elif page.route == "/page2":
+            page.views.clear()
+            page.views.append(ft.View("/page2", [page2(page)]))
+        page.update()
+
+    # Configurações do aplicativo
+    page.on_route_change = route_change
+    page.go("/")  # Define a página inicial
+
+ft.app(target=main)
